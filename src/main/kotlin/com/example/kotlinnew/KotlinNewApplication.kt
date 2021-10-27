c6b09faed6a69b60da0c1f1839e6c097649a2f58
LeoLaurent <laurentj.leo@gmail.com>
2021-10-27 10:53:29 +0200

    create spring project

---
+++ src/main/kotlin/com/example/kotlinnew/KotlinNewApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinnew
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinNewApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinNewApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinnew/KotlinNewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinnew
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinNewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
